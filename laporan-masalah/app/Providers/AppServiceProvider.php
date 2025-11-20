9732fcd619d2066a96352207c04fa310571395fc
aziizmusyafa18 <luthfimusyafa18@gmail.com>
2025-11-20 12:05:11 +0700

    fix: Force HTTPS scheme for production assets

--- laporan-masalah/app/Providers/AppServiceProvider.php
+++ laporan-masalah/app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Gate::define('isDPA', function (User $user) {
             return $user->role === 'dpa';
         });
